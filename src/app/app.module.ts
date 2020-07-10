73d807c7c6eca9688355089d3577a41546a01f4f
Monica Santos <monicamarquesantos@gmail.com>
2020-07-10 12:24:01 -0300

    Login funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-login',
@@ -11,5 +11,18 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent{
+    senha = '';
+    usuario = '';
+    login = false;
+   
+    constructor(){
+        
+    }
+
+    OnInit(){}
+
+    onLogin(){
+        this.login = true;
+    }
 
 }
