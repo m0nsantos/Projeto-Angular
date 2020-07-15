07a809d27c56b9d312706371c3b208fde5e4f042
Monica Santos <monicamarquesantos@gmail.com>
2020-07-15 10:14:05 -0300

    Atualizado a pagina home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,18 +6,43 @@ import { Component, OnInit, Input, HostListener } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  getLikes = [{number: 1}]
+  getLikes  = [{ number: 1 }];
+  getLikes1 = [{ number: 1 }];
+  getLikes2 = [{ number: 1 }];
+  getLikes3 = [{ number: 1 }];
+  getLikes4 = [{ number: 1 }];
+  getLikes5 = [{ number: 1 }];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
   onLike() {
     this.getLikes[0].number += 1;
+   }
+
+  onLike1() {
+   this.getLikes1[0].number += 1;
   }
 
+  onLike2() {
+    this.getLikes2[0].number += 1;
+   }
+
+  onLike3() {
+    this.getLikes3[0].number += 1;
+   }
+
+  onLike4() {
+   this.getLikes4[0].number += 1;
+  }
+
+  onLike5() {
+    this.getLikes5[0].number += 1;
+   }
+
+
+
 }
-
-
-  
